cb4ee5d34a0f6f55cbbcda427e981a8cf0d7e34d
anuabarnab4 <anuabarna.sf4019@gmail.com>
2023-07-10 14:28:10 +0530

    updated EmbedProperties file

--- IframeEmbedding/Models/EmbedProperties.cs
+++ IframeEmbedding/Models/EmbedProperties.cs
@@ -2,10 +2,10 @@
 {
     public class EmbedProperties
     {
-        public static string UserEmail = "anuabarna.b@syncfusion.com";
+        public static string UserEmail = "";
 
-        public static string EmbedSecret = "ctDCQJ8Ees7B2d8qMwVdH4e2WAT4ZCkL";
+        public static string EmbedSecret = "";
 
-        public static string DashboardUrl = "http://localhost:58295/bi/site/site1/dashboards/1f458e72-d598-47ca-a895-8a0f3430c3a5/Agile/Team%20Management%20in%20Agile";// URL of the dashboard to be embedded
+        public static string DashboardUrl = "";// URL of the dashboard to be embedded
     }
 }
