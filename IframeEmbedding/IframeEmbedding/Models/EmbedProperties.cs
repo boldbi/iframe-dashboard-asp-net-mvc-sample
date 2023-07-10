73bff2b1c5487c713f68b6e6911a2e231054e08e
anuabarnab4 <anuabarna.sf4019@gmail.com>
2023-07-10 12:52:15 +0530

    Updated sample

---
+++ IframeEmbedding/IframeEmbedding/Models/EmbedProperties.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IframeEmbedding.Models
+{
+    public class EmbedProperties
+    {
+        public static string UserEmail = "anuabarna.b@syncfusion.com";
+
+        public static string EmbedSecret = "ctDCQJ8Ees7B2d8qMwVdH4e2WAT4ZCkL";
+
+        public static string DashboardUrl = "http://localhost:58295/bi/site/site1/dashboards/1f458e72-d598-47ca-a895-8a0f3430c3a5/Agile/Team%20Management%20in%20Agile";// URL of the dashboard to be embedded
+    }
+}
